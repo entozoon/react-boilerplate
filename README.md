61864a05c63ebf3a4fe53e9f254392f2a281fe92
MichaelCook <entozoon@gmail.com>
2017-04-27 19:29:11 +0100

    Gone a bit weird but, I'll get there

---
+++ README.md
@@ -0,0 +1,11 @@
+
+## Creation
+
+npm install -g yo
+npm install -g generator-react-webpack
+yo react-webpack
+
+## Dev
+
+npm start
+http://localhost:8000/webpack-dev-server/
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,14 +3,30 @@ require('styles/App.css');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+class FoodThoughts extends React.Component {
+  static defaultProps = {
+    flavour: 'cold'
+  }
+  render() {
+    const food = 'pizza';
+    //const { flavour } = this.props;
+
+    return (
+      <div>
+        <p>{`I like ${food}`}</p>
+        <p>Specifically, {flavour} pizza</p>
+      </div>
+    );
+  }
+}
 
 class AppComponent extends React.Component {
   render() {
+    const title = 'Wooh';
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+        <h1>{title}</h1>
+        <FoodThoughts flavour="margarita" />
       </div>
     );
   }
